3c9b37c4fbcfaf6134427316830c2d1f876f1be3
no-name3 <hajatyanvahe@gmail.com>
2022-12-18 18:49:58 -0800

    delete proxi

--- src/redux/action/pizzas.js
+++ src/redux/action/pizzas.js
@@ -1,16 +1,21 @@
 import axios from "axios";
 
-export const setLoaded = payload =>( {
-  type: 'SET_LOADED',
-  payload
-})
+export const setLoaded = (payload) => ({
+  type: "SET_LOADED",
+  payload,
+});
 
-
-export const fetchPizzas = (category, sortBy) => (dispatch) =>{
-  dispatch(setLoaded(false))
-  axios.get(`/pizzas?${category !== 0 ? `category=${category}` : ""}&_sort=${sortBy.type}&_order=${sortBy.order}`).then(({ data }) => {
-    dispatch(setPizzas(data));
-  });
+export const fetchPizzas = (category, sortBy) => (dispatch) => {
+  dispatch(setLoaded(false));
+  axios
+    .get(
+      `https://breakable-red-drill.cyclic.app/pizzas?${
+        category !== 0 ? `category=${category}` : ""
+      }&_sort=${sortBy.type}&_order=${sortBy.order}`
+    )
+    .then(({ data }) => {
+      dispatch(setPizzas(data));
+    });
 };
 
 export const setPizzas = (items) => ({
